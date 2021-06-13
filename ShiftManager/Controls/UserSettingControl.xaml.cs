9551c2abcb5d13155825a8907d25fa51445b6dce
ＴＲ <public@technotter.com>
2021-06-14 03:12:13 +0900

    Add Controls

---
+++ ShiftManager/Controls/UserSettingControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ShiftManager.Controls
+{
+  /// <summary>
+  /// Interaction logic for UserSettingControl.xaml
+  /// </summary>
+  public partial class UserSettingControl : UserControl
+  {
+    public UserSettingControl()
+    {
+      InitializeComponent();
+    }
+  }
+}
