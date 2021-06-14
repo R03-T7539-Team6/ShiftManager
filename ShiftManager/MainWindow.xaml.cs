aeee0f35571ade57bdc7dea463737c65a59c044d
ＴＲ <public@technotter.com>
2021-06-15 05:23:18 +0900

    ページへの要素・一部機能実装

--- ShiftManager/MainWindow.xaml.cs
+++ ShiftManager/MainWindow.xaml.cs
@@ -1,4 +1,11 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+using Reactive.Bindings;
+
+using ShiftManager.DataClasses;
 
 namespace ShiftManager
 {
@@ -10,6 +17,29 @@ namespace ShiftManager
     public MainWindow()
     {
       InitializeComponent();
+      MainWindowViewModel mwvm = new() { MainFramePageChanger = new(MainFrame) };
+      DataContext = mwvm;
+      
     }
+  }
+
+  internal class MainWindowViewModel
+  {
+    public FramePageChanger MainFramePageChanger { get; init; }
+    public ReactivePropertySlim<bool> IsMenuExpanded { get; } = new(true);
+    public ReactivePropertySlim<NameData> UserName { get; } = new(new("FIRSTNAME", "LASTNAME"));
+  }
+
+  /// <summary>指定のFrameに, CommandParmeterで指定された型のPageを表示する</summary>
+  internal class FramePageChanger : ICommand
+  {
+    public event EventHandler CanExecuteChanged;
+    public Frame TargetFrame { get; }
+
+    public FramePageChanger(Frame _TargetFrame) => TargetFrame = _TargetFrame;
+
+    public bool CanExecute(object parameter) => TargetFrame is not null && parameter is Type;
+
+    public void Execute(object parameter) => TargetFrame.Content = (parameter as Type)?.GetConstructor(new Type[0]).Invoke(null);
   }
 }
