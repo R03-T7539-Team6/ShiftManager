c8f75b86b3148709bba95a9e25fd9292f8215dac
ＴＲ <public@technotter.com>
2021-06-15 04:02:13 +0900

    Update WorkLog.cs

--- ShiftManager.DataClasses/WorkLog.cs
+++ ShiftManager.DataClasses/WorkLog.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace ShiftManager.DataClasses
 {
-  public record WorkLog(UserID UserID, );
+  public record WorkLog(UserID UserID, SortedDictionary<DateTime, SingleWorkLog> WorkLogDictionary);
 
   public record SingleWorkLog(DateTime AttendanceTime, DateTime LeavingTime, Dictionary<DateTime, int> BreakTimeDictionary);
 }
