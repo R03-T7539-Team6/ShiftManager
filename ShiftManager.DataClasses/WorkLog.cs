0ea39e12f98a6559612f135744f3313f9ec89d4f
systemkaihaturonn1234 <85974501+systemkaihaturonn1234@users.noreply.github.com>
2021-06-16 11:09:11 +0900

    adfdf

--- ShiftManager.DataClasses/WorkLog.cs
+++ ShiftManager.DataClasses/WorkLog.cs
@@ -5,5 +5,7 @@ namespace ShiftManager.DataClasses
 {
   public record WorkLog(UserID UserID, SortedDictionary<DateTime, SingleWorkLog> WorkLogDictionary);
 
-  public record SingleWorkLog(DateTime AttendanceTime, DateTime LeavingTime, Dictionary<DateTime, int> BreakTimeDictionary);
+
 }
+
+qwertyuiop
