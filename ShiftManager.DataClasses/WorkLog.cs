484ca2783880a2f8ccf18b4fff45476e40ff0244
ＴＲ <public@technotter.com>
2021-06-16 11:27:20 +0900

    Update WorkLog.cs
    - ISingleWorkLogを追加
    - 各recordにinterfaceを設定

--- ShiftManager.DataClasses/WorkLog.cs
+++ ShiftManager.DataClasses/WorkLog.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 
 namespace ShiftManager.DataClasses
 {
-  public record WorkLog(UserID UserID, SortedDictionary<DateTime, SingleWorkLog> WorkLogDictionary);
+  public record WorkLog(UserID UserID, SortedDictionary<DateTime, SingleWorkLog> WorkLogDictionary) : IWorkLog;
 
-  public record SingleWorkLog(DateTime AttendanceTime, DateTime LeavingTime, Dictionary<DateTime, int> BreakTimeDictionary);
+  public record SingleWorkLog(DateTime AttendanceTime, DateTime LeavingTime, Dictionary<DateTime, int> BreakTimeDictionary) : ISingleWorkLog;
 
   public interface IWorkLog
   {
     UserID UserID { get; }
     SortedDictionary<DateTime, SingleWorkLog> WorkLogDictionary { get; }
+  }
+
+  public interface ISingleWorkLog
+  {
+    DateTime AttendanceTime { get; }
+    DateTime LeavingTime { get; }
+    Dictionary<DateTime, int> BreakTimeDictionary { get; }
   }
 }
