1d9bedf0f48263e8448982b7e7b09898d3b13c4d
ＴＲ <public@technotter.com>
2021-06-23 07:25:50 +0900

    シフト希望操作テスト用メソッド群を実装

--- ShiftManager.Communication/InternalApi.ShiftRequest.cs
+++ ShiftManager.Communication/InternalApi.ShiftRequest.cs
@@ -7,6 +7,7 @@ namespace ShiftManager.Communication
 {
   public interface InternalApi_ShiftRequest
   {
+    ApiResult AddShiftRequest(ISingleShiftData singleShiftData);
     Task<ApiResult> AddShiftRequestAsync(ISingleShiftData singleShiftData);
     Task<ApiResult<SingleShiftData>> GetShiftRequestByDateAsync(DateTime date);
     Task<ApiResult> UpdateShiftRequestAsync(ISingleShiftData singleShiftData);
@@ -14,19 +15,59 @@ namespace ShiftManager.Communication
 
   public partial class InternalApi : InternalApi_ShiftRequest
   {
+    /// <summary>シフト希望を追加します  初めての追加の場合は, コレクションにユーザのデータが生成されたうえで追加されます</summary>
+    /// <param name="singleShiftData">追加する単一シフトデータ</param>
+    /// <returns>実行結果</returns>
+    public ApiResult AddShiftRequest(ISingleShiftData singleShiftData)
+    {
+      IUserID targetUserID = singleShiftData.UserID;
+      if (!TestD.ShiftRequestsDictionary.TryGetValue(targetUserID, out IShiftRequest? shiftRequest) || shiftRequest is null)
+      {
+        TestD.ShiftRequestsDictionary.Add(targetUserID, new ShiftRequest(targetUserID, DateTime.Now, new() { { singleShiftData.WorkDate, singleShiftData } }));
+
+        return new(true, ApiResultCodes.UserID_Not_Found);
+      }
+      else
+      {
+        try
+        {
+          TestD.ShiftRequestsDictionary[targetUserID] = new ShiftRequest(shiftRequest) with
+          {
+            LastUpdate = DateTime.Now,
+            RequestsDictionary = new(shiftRequest.RequestsDictionary) { { singleShiftData.WorkDate, singleShiftData } }
+          };
+
+          return new(true, ApiResultCodes.Success);
+        }
+        catch (ArgumentNullException)
+        {
+          return new(false, ApiResultCodes.NewData_Is_NULL);
+        }
+        catch (System.Collections.Generic.KeyNotFoundException)
+        {
+          return new(false, ApiResultCodes.Target_Date_Not_Found);
+        }
+      } 
+    }
     public Task<ApiResult> AddShiftRequestAsync(ISingleShiftData singleShiftData)
-    {
-      throw new NotImplementedException();
-    }
+      => Task.Run(() => AddShiftRequest(singleShiftData));
 
-    public Task<ApiResult<SingleShiftData>> GetShiftRequestByDateAsync(DateTime date)
+    public Task<ApiResult<SingleShiftData>> GetShiftRequestByDateAsync(DateTime date) => Task.Run<ApiResult<SingleShiftData>>(() =>
     {
-      throw new NotImplementedException();
-    }
+      if (CurrentUserData is null)
+        return new(false, ApiResultCodes.Not_Logged_In, null);//ログイン中しか使用できない
 
+      if (TestD.ShiftRequestsDictionary.TryGetValue(CurrentUserData.UserID, out IShiftRequest? shiftRequest) || shiftRequest is null)
+        return new(false, ApiResultCodes.UserID_Not_Found, null);
+
+      if (shiftRequest.RequestsDictionary.TryGetValue(date, out ISingleShiftData? singleShiftData) || singleShiftData is null)
+        return new(false, ApiResultCodes.Target_Date_Not_Found, null);
+      else
+        return new(true, ApiResultCodes.Success, new(singleShiftData));
+    });
+
+    [Obsolete("Please use AddShiftRequestAsync Method")]
     public Task<ApiResult> UpdateShiftRequestAsync(ISingleShiftData singleShiftData)
-    {
-      throw new NotImplementedException();
-    }
+      => AddShiftRequestAsync(singleShiftData);
   }
 }
