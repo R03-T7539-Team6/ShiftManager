a9cc29615e7c29eda99ad5b4bd8a114bf07ab923
ＴＲ <public@technotter.com>
2021-06-23 00:02:39 +0900

    Create InternalApi.UserData.cs

---
+++ ShiftManager.Communication/InternalApi.UserData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using ShiftManager.DataClasses;
+
+namespace ShiftManager.Communication
+{
+  public interface IInternalApi_UserData
+  {
+    Task<ApiResult> CheckPasswordValid(HashedPasswordGetter hashedPasswordGetter);
+    Task<ApiResult> UpdatePassword(HashedPasswordGetter hashedPasswordGetter);
+
+    Task<ApiResult<IWorkLog>> GetWorkLog();
+    Task<ApiResult<IUserSetting>> GetUserSetting();
+  }
+
+  public partial class InternalApi : IInternalApi_UserData
+  {
+    public Task<ApiResult> CheckPasswordValid(HashedPasswordGetter hashedPasswordGetter)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<ApiResult<IUserSetting>> GetUserSetting()
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<ApiResult<IWorkLog>> GetWorkLog()
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<ApiResult> UpdatePassword(HashedPasswordGetter hashedPasswordGetter)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
