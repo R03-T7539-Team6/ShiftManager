a9b89ada7fb39baf3ed5463eb078c110f09dd0a7
ＴＲ <public@technotter.com>
2021-06-23 00:32:56 +0900

    Shift Request Apiインターフェイスを追加

---
+++ ShiftManager.Communication/InternalApi.ShiftRequest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using ShiftManager.DataClasses;
+
+namespace ShiftManager.Communication
+{
+  public interface InternalApi_ShiftRequest
+  {
+    Task<ApiResult> AddShiftRequestAsync(ISingleShiftData singleShiftData);
+    Task<ApiResult<SingleShiftData>> GetShiftRequestByDateAsync(DateTime date);
+    Task<ApiResult> UpdateShiftRequestAsync(ISingleShiftData singleShiftData);
+  }
+
+  public partial class InternalApi : InternalApi_ShiftRequest
+  {
+    public Task<ApiResult> AddShiftRequestAsync(ISingleShiftData singleShiftData)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<ApiResult<SingleShiftData>> GetShiftRequestByDateAsync(DateTime date)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<ApiResult> UpdateShiftRequestAsync(ISingleShiftData singleShiftData)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
