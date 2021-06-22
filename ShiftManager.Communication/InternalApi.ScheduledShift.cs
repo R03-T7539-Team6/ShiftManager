753e857fa46f52ad9e054bdc7d1d2c08eb39f720
ＴＲ <public@technotter.com>
2021-06-23 06:44:55 +0900

    ScheduledShift関連のUIテスト用実装追加

--- ShiftManager.Communication/InternalApi.ScheduledShift.cs
+++ ShiftManager.Communication/InternalApi.ScheduledShift.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 using ShiftManager.DataClasses;
@@ -8,7 +9,7 @@ namespace ShiftManager.Communication
 {
   public interface InternalApi_ScheduledShift
   {
-    Task<ApiResult<SingleShiftData>> GetScheduledShiftByIDAsync(DateTime targetDate, UserID userID);
+    Task<ApiResult<SingleShiftData>> GetScheduledShiftByIDAsync(DateTime targetDate, IUserID userID);
     Task<ApiResult> UpdateShiftSchedulingStateAsync(DateTime targetDate, ShiftSchedulingState shiftSchedulingState);
     Task<ApiResult> UpdateSingleScheduledShiftListAsync(DateTime targetDate, IReadOnlyCollection<ISingleShiftData> singleShiftDatas);
     Task<ApiResult> UpdateRequiredWorkerCountListAsync(DateTime targetDate, IReadOnlyCollection<int> singleShiftDatas);
@@ -16,24 +17,58 @@ namespace ShiftManager.Communication
 
   public partial class InternalApi : InternalApi_ScheduledShift
   {
-    public Task<ApiResult<SingleShiftData>> GetScheduledShiftByIDAsync(DateTime targetDate, UserID userID)
+    /// <summary>指定日の予定シフトを, ユーザID指定で取得します</summary>
+    /// <param name="targetDate">取得する予定シフトの対象日</param>
+    /// <param name="userID">取得する予定シフトのユーザID</param>
+    /// <returns>実行結果</returns>
+    public Task<ApiResult<SingleShiftData>> GetScheduledShiftByIDAsync(DateTime targetDate, IUserID userID) => Task.Run<ApiResult<SingleShiftData>>(() =>
     {
-      throw new NotImplementedException();
-    }
+      if (!TestD.ScheduledShiftDictionary.TryGetValue(targetDate, out IScheduledShift? scheduledShift) || scheduledShift is null)
+        return new(false, ApiResultCodes.Target_Date_Not_Found, null);
 
-    public Task<ApiResult> UpdateRequiredWorkerCountListAsync(DateTime targetDate, IReadOnlyCollection<int> singleShiftDatas)
-    {
-      throw new NotImplementedException();
-    }
+      if (!scheduledShift.ShiftDictionary.TryGetValue(userID, out ISingleShiftData? singleShiftData) || singleShiftData is null)
+        return new(false, ApiResultCodes.UserID_Not_Found, null);
+      else
+      {
+        ApiResultCodes resultCode = scheduledShift.SchedulingState switch
+        {
+          ShiftSchedulingState.FinalVersion => ApiResultCodes.Success,
+          ShiftSchedulingState.Working => ApiResultCodes.Scheduling_Is_Still_In_Working,
+          ShiftSchedulingState.NotStarted => ApiResultCodes.Scheduling_Is_Not_Started,
+          _ => ApiResultCodes.Unknown_Error,
+        };
+        return new(resultCode != ApiResultCodes.Unknown_Error, resultCode, new(singleShiftData));
+      }
+    });
+
+    public Task<ApiResult> UpdateRequiredWorkerCountListAsync(DateTime targetDate, IReadOnlyCollection<int> requiredWorkerCountList)
+      => Task.Run(() => UpdateScheduledShift(targetDate, (i) => new ScheduledShift(i) with { RequiredWorkerCountList = new(requiredWorkerCountList) }));
 
     public Task<ApiResult> UpdateShiftSchedulingStateAsync(DateTime targetDate, ShiftSchedulingState shiftSchedulingState)
-    {
-      throw new NotImplementedException();
-    }
+      => Task.Run(() => UpdateScheduledShift(targetDate, (i) => new ScheduledShift(i) with { SchedulingState = shiftSchedulingState }));
 
     public Task<ApiResult> UpdateSingleScheduledShiftListAsync(DateTime targetDate, IReadOnlyCollection<ISingleShiftData> singleShiftDatas)
+      => Task.Run(() => UpdateScheduledShift(targetDate, (i) => new ScheduledShift(i) with { ShiftDictionary = singleShiftDatas.ToDictionary(i => i.UserID) }));
+
+
+    private ApiResult UpdateScheduledShift(DateTime targetDate, Func<IScheduledShift, ScheduledShift> DataUpdater)
     {
-      throw new NotImplementedException();
+      if (!TestD.ScheduledShiftDictionary.TryGetValue(targetDate, out IScheduledShift? scheduledShift) || scheduledShift is null)
+        return new(false, ApiResultCodes.Target_Date_Not_Found);
+      var newData = DataUpdater.Invoke(scheduledShift);
+      try
+      {
+        TestD.ScheduledShiftDictionary[targetDate] = newData;
+        return new(true, ApiResultCodes.Success);
+      }
+      catch (KeyNotFoundException)
+      {
+        return new(false, ApiResultCodes.Target_Date_Not_Found);
+      }
+      catch (ArgumentNullException)
+      {
+        return new(false, ApiResultCodes.NewData_Is_NULL);
+      }
     }
   }
 }
