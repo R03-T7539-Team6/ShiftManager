9d5c3a408b78549772db817269d3dc6dccc4a4a2
ＴＲ <public@technotter.com>
2021-06-23 00:32:40 +0900

    Scheduled Shift Apiインターフェイスを追加

---
+++ ShiftManager.Communication/InternalApi.ScheduledShift.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using ShiftManager.DataClasses;
+
+namespace ShiftManager.Communication
+{
+  public interface InternalApi_ScheduledShift
+  {
+    Task<ApiResult<SingleShiftData>> GetScheduledShiftByIDAsync(DateTime targetDate, UserID userID);
+    Task<ApiResult> UpdateShiftSchedulingStateAsync(DateTime targetDate, ShiftSchedulingState shiftSchedulingState);
+    Task<ApiResult> UpdateSingleScheduledShiftListAsync(DateTime targetDate, IReadOnlyCollection<ISingleShiftData> singleShiftDatas);
+    Task<ApiResult> UpdateRequiredWorkerCountListAsync(DateTime targetDate, IReadOnlyCollection<int> singleShiftDatas);
+  }
+
+  public partial class InternalApi : InternalApi_ScheduledShift
+  {
+    public Task<ApiResult<SingleShiftData>> GetScheduledShiftByIDAsync(DateTime targetDate, UserID userID)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<ApiResult> UpdateRequiredWorkerCountListAsync(DateTime targetDate, IReadOnlyCollection<int> singleShiftDatas)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<ApiResult> UpdateShiftSchedulingStateAsync(DateTime targetDate, ShiftSchedulingState shiftSchedulingState)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<ApiResult> UpdateSingleScheduledShiftListAsync(DateTime targetDate, IReadOnlyCollection<ISingleShiftData> singleShiftDatas)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
