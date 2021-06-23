3011b080e33b11412bf11d62fcfa5916a5a6a107
ＴＲ <public@technotter.com>
2021-06-24 00:44:54 +0900

    パスワード強度表示コントロールを実装

--- ShiftManager/Controls/PasswordStrengthVisualizerControl.xaml.cs
+++ ShiftManager/Controls/PasswordStrengthVisualizerControl.xaml.cs
@@ -9,15 +9,18 @@ namespace ShiftManager.Controls
     static public DependencyProperty PasswordTextProperty = DependencyProperty.Register(nameof(PasswordText), typeof(string), typeof(PasswordStrengthVisualizerControl), new(PWChanged));
 
     public double PasswordStrength { get => (double)GetValue(PasswordStrengthProperty); protected set => SetValue(PasswordStrengthPropertyKey, value); }
-    static private DependencyPropertyKey PasswordStrengthPropertyKey = DependencyProperty.RegisterReadOnly(nameof(PasswordStrength), typeof(double), typeof(UserSettingControl), new());
+    static private DependencyPropertyKey PasswordStrengthPropertyKey = DependencyProperty.RegisterReadOnly(nameof(PasswordStrength), typeof(double), typeof(PasswordStrengthVisualizerControl), new());
     static public DependencyProperty PasswordStrengthProperty = PasswordStrengthPropertyKey.DependencyProperty;
 
-    static PropertyChangedCallback PWChanged { get; } = (d, e) =>
-    {
+    /// <summary>パスワードの強度における"良い"の閾値</summary>
+    public static double PWStrength_Good { get; } = 0.7;
+    /// <summary>パスワードの強度における"最低限"の閾値</summary>
+    public static double PWStrength_Least { get; } = 0.4;
 
-    };
+    static void PWChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) => (d as PasswordStrengthVisualizerControl).PasswordStrength = GetPasswordStrength(e.NewValue as string);
 
     static PasswordStrengthVisualizerControl() => DefaultStyleKeyProperty.OverrideMetadata(typeof(PasswordStrengthVisualizerControl), new FrameworkPropertyMetadata(typeof(PasswordStrengthVisualizerControl)));
-    
+
+    static public double GetPasswordStrength(in string s) => string.IsNullOrWhiteSpace(s) ? 0.1 : 0.2 + (double)Zxcvbn.Core.EvaluatePassword(s as string).Score / 5;//Scoreの最大値が4のため
   }
 }
