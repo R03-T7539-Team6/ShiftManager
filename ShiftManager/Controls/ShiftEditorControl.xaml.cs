7b754148819cfa7a37b93ddeba8a0e5c5ddb1cb8
ＴＲ <public@technotter.com>
2021-06-29 07:17:01 +0900

    依存関係プロパティ等を実装

--- ShiftManager/Controls/ShiftEditorControl.xaml.cs
+++ ShiftManager/Controls/ShiftEditorControl.xaml.cs
@@ -1,11 +1,91 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+
+using ShiftManager.DataClasses;
 
 namespace ShiftManager.Controls
 {
-  public class ShiftEditorControl : Control
+  public class ShiftEditorControl : ContentControl, ISingleShiftData
   {
+    public Elements VisibleElements { get => (Elements)GetValue(VisibleElementsProperty); set => SetValue(VisibleElementsProperty, value); }
+    static public DependencyProperty VisibleElementsProperty = DependencyProperty.Register(nameof(VisibleElements), typeof(Elements), typeof(ShiftEditorControl));
+
+    public Elements EditableElements { get => (Elements)GetValue(EditableElementsProperty); set => SetValue(EditableElementsProperty, value); }
+    static public DependencyProperty EditableElementsProperty = DependencyProperty.Register(nameof(EditableElements), typeof(Elements), typeof(ShiftEditorControl));
+
+    public DateTime WorkDate { get => (DateTime)GetValue(WorkDateProperty); set => SetValue(WorkDateProperty, value); }
+    static public DependencyProperty WorkDateProperty = DependencyProperty.Register(nameof(WorkDate), typeof(DateTime), typeof(ShiftEditorControl));
+    public DateTime AttendanceTime { get => (DateTime)GetValue(AttendanceTimeProperty); set => SetValue(AttendanceTimeProperty, value); }
+    static public DependencyProperty AttendanceTimeProperty = DependencyProperty.Register(nameof(AttendanceTime), typeof(DateTime), typeof(ShiftEditorControl));
+    public DateTime LeavingTime { get => (DateTime)GetValue(LeavingTimeProperty); set => SetValue(LeavingTimeProperty, value); }
+    static public DependencyProperty LeavingTimeProperty = DependencyProperty.Register(nameof(LeavingTime), typeof(DateTime), typeof(ShiftEditorControl));
+
+    public Dictionary<DateTime, int> BreakTimeDictionary { get => (Dictionary<DateTime, int>)GetValue(BreakTimeDictionaryProperty); set => SetValue(BreakTimeDictionaryProperty, value); }
+    static public DependencyProperty BreakTimeDictionaryProperty = DependencyProperty.Register(nameof(BreakTimeDictionary), typeof(Dictionary<DateTime, int>), typeof(ShiftEditorControl));
+
+    public Brush AttendingTimeBrush { get => (Brush)GetValue(AttendingTimeBrushProperty); set => SetValue(AttendingTimeBrushProperty, value); }
+    static public DependencyProperty AttendingTimeBrushProperty = DependencyProperty.Register(nameof(AttendingTimeBrush), typeof(Brush), typeof(ShiftEditorControl));
+    public Brush BreakingTimeBrush { get => (Brush)GetValue(BreakingTimeBrushProperty); set => SetValue(BreakingTimeBrushProperty, value); }
+    static public DependencyProperty BreakingTimeBrushProperty = DependencyProperty.Register(nameof(BreakingTimeBrush), typeof(Brush), typeof(ShiftEditorControl));
+    public Brush ShiftEditBarBackground { get => (Brush)GetValue(ShiftEditBarBackgroundProperty); set => SetValue(ShiftEditBarBackgroundProperty, value); }
+    static public DependencyProperty ShiftEditBarBackgroundProperty = DependencyProperty.Register(nameof(ShiftEditBarBackground), typeof(Brush), typeof(ShiftEditorControl));
+
+    public double ShiftEditBarScale { get => (double)GetValue(ShiftEditBarScaleProperty); set => SetValue(ShiftEditBarScaleProperty, value); }
+    static public DependencyProperty ShiftEditBarScaleProperty = DependencyProperty.Register(nameof(ShiftEditBarScale), typeof(double), typeof(ShiftEditorControl));
+
+    public IUserID UserID { get; private set; }
+    public bool IsPaidHoliday { get; private set; }
+
+
+
+
     static ShiftEditorControl() => DefaultStyleKeyProperty.OverrideMetadata(typeof(ShiftEditorControl), new FrameworkPropertyMetadata(typeof(ShiftEditorControl)));
 
+    public ShiftEditorControl() { }
+    public ShiftEditorControl(ISingleShiftData i) => SingleShiftData = i;
+
+    public ISingleShiftData SingleShiftData
+    {
+      get => new SingleShiftData(this);
+      set
+      {
+        UserID = value.UserID;
+        WorkDate = value.WorkDate;
+        IsPaidHoliday = value.IsPaidHoliday;
+        AttendanceTime = value.AttendanceTime;
+        LeavingTime = value.LeavingTime;
+        BreakTimeDictionary = new(value.BreakTimeDictionary);
+      }
+    }
+
+
+    [Flags]
+    public enum Elements
+    {
+      None = 0,
+      /// <summary>テキスト</summary>
+      Text = 1 << 1,
+
+      /// <summary>出勤時刻</summary>
+      AttendTime = 1 << 2,
+
+      /// <summary>退勤時刻</summary>
+      LeaveTime = 1 << 3,
+
+      /// <summary>休憩時分</summary>
+      BraakTime = 1 << 4,
+
+      /// <summary>シフト表示UI (バー型シフト視覚化要素)</summary>
+      ShiftBar = 1 << 5,
+
+      /// <summary>勤務時間長</summary>
+      WorkTimeLen = 1 << 6,
+
+      /// <summary>削除ボタン</summary>
+      DeleteButton = 1 << 7
+    }
   }
 }
