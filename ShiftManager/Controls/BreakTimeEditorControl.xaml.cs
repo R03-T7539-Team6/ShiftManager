1482f672073580d9aa822460dcf18ee94fef76f0
ＴＲ <public@technotter.com>
2021-06-29 21:54:51 +0900

    BreakTimeEditorコントロールを追加

---
+++ ShiftManager/Controls/BreakTimeEditorControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace ShiftManager.Controls
+{
+  public class BreakTimeEditorControl : Control
+  {
+
+    static BreakTimeEditorControl() => DefaultStyleKeyProperty.OverrideMetadata(typeof(BreakTimeEditorControl), new FrameworkPropertyMetadata(typeof(BreakTimeEditorControl)));
+    
+  }
+}
