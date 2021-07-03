84a09eca9e48cee3c4f7d7f71d8b16e5624842d1
ＴＲ <public@technotter.com>
2021-07-03 18:56:13 +0900

    コピーコンストラクタにNULLが渡された際の処理を実装

--- ShiftManager.DataClasses/UserData.cs
+++ ShiftManager.DataClasses/UserData.cs
@@ -82,7 +82,7 @@ namespace ShiftManager.DataClasses
     public event PropertyChangedEventHandler? PropertyChanged;
 
     public NameData_PropertyChanged() { }
-    public NameData_PropertyChanged(INameData i) { FirstName = i.FirstName; LastName = i.LastName; }
+    public NameData_PropertyChanged(INameData i) { FirstName = i?.FirstName ?? string.Empty; LastName = i?.LastName ?? string.Empty; }
 
     [AutoNotify]
     private string _FirstName = string.Empty;
