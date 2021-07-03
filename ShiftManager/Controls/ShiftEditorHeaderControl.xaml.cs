aebb543c5f0869e01347e462aa9a415b9685da72
ＴＲ <public@technotter.com>
2021-07-03 19:34:33 +0900

    ShiftEditorHeaderControlを実装

---
+++ ShiftManager/Controls/ShiftEditorHeaderControl.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ShiftManager.Controls
+{
+  public class ShiftEditorHeaderControl : ContentControl
+  {
+    public ShiftEditorElements VisibleElements { get => (ShiftEditorElements)GetValue(VisibleElementsProperty); set => SetValue(VisibleElementsProperty, value); }
+    public static readonly DependencyProperty VisibleElementsProperty = DependencyProperty.Register(nameof(VisibleElements), typeof(ShiftEditorElements), typeof(ShiftEditorHeaderControl));
+
+    static ShiftEditorHeaderControl() => DefaultStyleKeyProperty.OverrideMetadata(typeof(ShiftEditorHeaderControl), new FrameworkPropertyMetadata(typeof(ShiftEditorHeaderControl)));
+    
+  }
+}
