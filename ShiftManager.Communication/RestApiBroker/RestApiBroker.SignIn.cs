a42a5c773299caa2015165c032b3663385cf97bf
ＴＲ <public@technotter.com>
2021-07-04 22:21:41 +0900

    機能を実装

--- ShiftManager.Communication/RestApiBroker/RestApiBroker.SignIn.cs
+++ ShiftManager.Communication/RestApiBroker/RestApiBroker.SignIn.cs
@@ -11,34 +11,56 @@ namespace ShiftManager.Communication
     /// <param name="userID">試行するUserID</param>
     /// <param name="hashedPassword">試行するハッシュ化パスワード</param>
     /// <returns>試行結果</returns>
-    public Task<ApiResult> SignInAsync(IUserID userID, IHashedPassword hashedPassword) => Task.Run<ApiResult>(() =>
+    public async Task<ApiResult> SignInAsync(IUserID userID, IHashedPassword hashedPassword)
     {
       if(string.IsNullOrWhiteSpace(userID?.Value))
         return new(false, ApiResultCodes.UserID_Not_Found);
       if(string.IsNullOrWhiteSpace(hashedPassword?.Hash))
         return new(false, ApiResultCodes.Password_Not_Match);
 
-      if (!TestD.UserDataDictionary.TryGetValue(new(userID), out IUserData? userD))
-        return new(false, ApiResultCodes.UserID_Not_Found);
+      if (userID.Value.Length != 8)
+        return new(false, ApiResultCodes.Invalid_Length_UserID);
 
-      if (userD.HashedPassword.Hash == hashedPassword.Hash)
-      {
-        CurrentUserData = new UserData(userD) with { HashedPassword = new HashedPassword() };
-        return new(true, ApiResultCodes.Success);//実際はここでトークンをキャッシュする
-      }
-      else
-        return new(false, ApiResultCodes.Password_Not_Match);
-    });
+      var result = await Api.ExecuteWithDataAsync<UserIDPW, SignInReturn>("/login", new(userID.Value, hashedPassword.Hash));
+
+      if (!result.IsSuccess || string.IsNullOrWhiteSpace(result.ReturnData?.token))
+        return new(false, ApiResultCodes.SignIn_Failed);
+
+      Api.Token = result.ReturnData.token;
+
+      var userData = await GetUserDataByIDAsync(userID);
+      if (!userData.IsSuccess)
+        return new(false, ApiResultCodes.SignIn_Failed);
+
+      CurrentUserData = userData.ReturnData;
+
+      return new(true, ApiResultCodes.Success);
+    }
 
     /// <summary>サインアウトを実行します</summary>
     /// <returns>実行結果</returns>
     public Task<ApiResult> SignOutAsync() => Task.Run<ApiResult>(() =>
     {
+      Api.Token = string.Empty;
+
       if (CurrentUserData is null)
         return new(false, ApiResultCodes.Not_Logged_In);
 
       CurrentUserData = null;
       return new(true, ApiResultCodes.Success);
     });
+
+    public class UserIDPW
+    {
+      public UserIDPW() { }
+      public UserIDPW(in string userid, in string pw) { user_id = userid; password = pw; }
+      public string user_id { get; set; } = string.Empty;
+      public string password { get; set; } = string.Empty;
+    }
+    public class SignInReturn
+    {
+      public string token { get; set; } = string.Empty;
+    }
   }
+
 }
