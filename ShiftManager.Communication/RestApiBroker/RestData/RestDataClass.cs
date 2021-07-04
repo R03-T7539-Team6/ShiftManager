ebfa6a3a64869c5720a2eeeecbf68e2e350715d7
ＴＲ <public@technotter.com>
2021-07-04 11:55:50 +0900

    REST API用データ型を定義

---
+++ ShiftManager.Communication/RestApiBroker/RestData/RestDataClass.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace ShiftManager.Communication.RestApiBroker.RestData
+{
+  public class RestModel
+  {
+    public uint ID { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public DateTime DeletedAt { get; set; }
+  }
+
+  public class RestUser : RestModel
+  {
+    public string UserID { get; set; }
+    public string StoreID { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string UserState { get; set; }
+    public string UserGroup { get; set; }
+  }
+
+  public class RestShift
+  {
+    public uint ID { get; set; }
+    public string UserID { get; set; }
+    public string StoreID { get; set; }
+    public DateTime WokrDate { get; set; }
+    public bool IsPaidHoliday { get; set; }
+    public bool IsRequest { get; set; }
+    public DateTime AttendanceTime { get; set; }
+    public DateTime LeavingTime { get; set; }
+    public DateTime StartBreakTime { get; set; }
+    public DateTime EndBreakTime { get; set; }
+  }
+
+  public class RestShiftRequest : RestModel
+  {
+    public string UserID { get; set; }
+    public string StoreID { get; set; }
+    public DateTime LastUpdate { get; set; }
+    public RestShift[] Shift { get; set; }
+  }
+
+  public class RestShiftSchedule : RestModel
+  {
+    public string StoreID { get; set; }
+    public DateTime TargetDate { get; set; }
+    public DateTime StartOfSchedule { get; set; }
+    public DateTime EndOfSchedule { get; set; }
+    public string ShiftSchedulingState { get; set; }
+    public RestShift[] Shift { get; set; }
+    public uint WorkerNum { get; set; }
+  }
+
+  public class RestStore : RestModel
+  {
+    public string StoreID { get; set; }
+    public RestUser[] User { get; set; }
+    public RestShiftRequest[] ShiftRequest { get; set; }
+    public RestShiftSchedule[] ShiftSchedule { get; set; }
+  }
+
+  public class RestWorkLog : RestModel
+  {
+    public string UserID { get; set; }
+    public DateTime AttendanceTime { get; set; }
+    public DateTime LeavingTime { get; set; }
+    public DateTime StartBreakTime { get; set; }
+    public DateTime EndBreakTime { get; set; }
+  }
+}
---
+++ ShiftManager.Communication/RestApiBroker/RestData/RestDataConstants.cs
@@ -0,0 +1,31 @@
+﻿namespace ShiftManager.Communication.RestApiBroker.RestData
+{
+  public static class RestDataConstants
+  {
+    public static class Status
+    {
+      public static readonly string Normal = "Normal";
+      public static readonly string InLeaveOfAbsence = "InLeaveOfAbsence";
+      public static readonly string Retired = "Retired";
+      public static readonly string NotHired = "NotHired";
+      public static readonly string Others = "Others";
+    }
+
+    public static class Group
+    {
+      public static readonly string None = "None";
+      public static readonly string SystemAdmin = "SystemAdmin";
+      public static readonly string SuperUser = "SuperUser";
+      public static readonly string NormalUser = "NormalUser";
+      public static readonly string ForTimeRecordTerminal = "ForTimeRecordTerminal";
+    }
+
+    public static class ShiftStatus
+    {
+      public static readonly string NoneShift = "None";
+      public static readonly string NotStarted = "NotStarted";
+      public static readonly string Working = "Working";
+      public static readonly string FinalVersion = "FinalVersion";
+    }
+  }
+}
