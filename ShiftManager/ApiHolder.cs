b5bd7371fa7523ea8d4c6e73dd8f1b80b87fb67a
ＴＲ <public@technotter.com>
2021-07-05 00:29:28 +0900

    プロパティのプロキシを作成

--- ShiftManager/ApiHolder.cs
+++ ShiftManager/ApiHolder.cs
@@ -1,15 +1,25 @@
 ﻿
 using ShiftManager.Communication;
+using ShiftManager.DataClasses;
 
 namespace ShiftManager
 {
   public class ApiHolder : IApiHolder
   {
-    public InternalApi Api { get; } = new();
+    //public InternalApi Api { get; } = new();
+    public RestApiBroker Api { get; } = new();
+    public StoreID CurrentStoreID { get => new StoreID("0000"); }
+    public UserID CurrentUserID { get => new(Api.CurrentUserData?.UserID); }
+    public NameData CurrentUserName { get => new(Api.CurrentUserData?.FullName); }
   }
   public interface IApiHolder
   {
-    InternalApi Api { get; }
+    //InternalApi Api { get; }
+    RestApiBroker Api { get; }
+
+    StoreID CurrentStoreID { get; }
+    public UserID CurrentUserID { get; }
+    public NameData CurrentUserName { get; }
   }
 
   public interface IContainsApiHolder
