fa613dc86e8f5ff1a8179a6f12272d068360a201
ＴＲ <public@technotter.com>
2021-07-04 19:06:29 +0900

    RestApiでの対話を専用クラスに分離

--- ShiftManager.Communication/RestApiBroker/RestApiBroker.Main.cs
+++ ShiftManager.Communication/RestApiBroker/RestApiBroker.Main.cs
@@ -1,6 +1,8 @@
 ﻿using System.Net;
 using System.Threading.Tasks;
 
+using Newtonsoft.Json;
+
 using RestSharp;
 using RestSharp.Authenticators;
 
@@ -11,66 +13,10 @@ namespace ShiftManager.Communication
 {
   public partial class RestApiBroker
   {
+    internal RemoteApi Api { get; } = new();
     public IUserData? CurrentUserData { get; private set; } = null;
-    private StoreData TestD { get; set; } = new SampleDataForStandaloneTest().StoreData;//To Debug
 
-    private string Token { get; set; } = string.Empty;
-
-
-    const string TargetURL = "https://immense-harbor-69424.herokuapp.com";
-
-    const int TimeOut = 5000;
-
-    internal async Task<ApiResult<T>> GetDataAsync<T>(string path) where T : class
-    {
-      var result = await GetJsonDataAsync(path);
-
-      return result.StatusCode == HttpStatusCode.OK ?
-        new(true, ApiResultCodes.Success, SimpleJson.DeserializeObject<T>(result.Content))
-        : new(false, ToApiRes(result.StatusCode), null);
-    }
-    internal async Task<(HttpStatusCode StatusCode, string Content)> GetJsonDataAsync(string path)
-    {
-      RestRequest request = new(path, Method.GET);
-
-      var result = await ExecuteApi(request);
-
-      return (result.StatusCode, result.Content);
-    }
-
-    internal async Task<ApiResult<TResult>> ExecuteWithDataAsync<TPost, TResult>(string path, TPost data)
-      where TPost : class
-      where TResult : class
-    {
-      RestRequest request = new(path, Method.POST);
-      request.AddJsonBody(data);
-
-      var result = await ExecuteApi(request);
-
-      return result.StatusCode == HttpStatusCode.OK ?
-        new(true, ApiResultCodes.Success, SimpleJson.DeserializeObject<TResult>(result.Content))
-        : new(false, ToApiRes(result.StatusCode), null);
-    }
-    internal async Task<ApiResult> ExecuteWithDataAsync<T>(string path, T data) where T : class
-    {
-      RestRequest request = new(path, Method.POST);
-      request.AddJsonBody(data);
-
-      var result = await ExecuteApi(request);
-
-      return new(result.StatusCode == HttpStatusCode.OK, ToApiRes(result.StatusCode));
-    }
-
-    internal Task<IRestResponse> ExecuteApi(IRestRequest request)
-    {
-      RestClient client = new(TargetURL);
-      client.Timeout = TimeOut;
-
-      if (!string.IsNullOrWhiteSpace(Token))
-        client.Authenticator = new JwtAuthenticator(Token);
-
-      return client.ExecuteAsync(request);
-    }
+    public bool IsLoggedIn { get => !string.IsNullOrWhiteSpace(Api.Token); }
 
     static ApiResultCodes ToApiRes(in HttpStatusCode statusCode) => statusCode switch
     {
@@ -81,5 +27,79 @@ namespace ShiftManager.Communication
       HttpStatusCode.NotFound => ApiResultCodes.E404_Not_Found,
       _ => ApiResultCodes.Unknown_Error
     };
+
+
+    internal class RemoteApi
+    {
+      public string Token { get; set; } = string.Empty;
+
+
+      const string TargetURL = "https://immense-harbor-69424.herokuapp.com";
+
+      const int TimeOut = 5000;
+      const string application_json = "application/json";
+
+      private static readonly JsonSerializerSettings jsonSerializerSettings = new()
+      {
+        DateTimeZoneHandling = DateTimeZoneHandling.Local
+      };
+      private static string ToJson<T>(in T obj) => JsonConvert.SerializeObject(obj, jsonSerializerSettings);
+
+      private static T? FromJson<T>(in string json) => JsonConvert.DeserializeObject<T>(json, jsonSerializerSettings);
+
+      public async Task<ApiResult<T>> GetDataAsync<T>(string path) where T : class
+      {
+        var result = await GetJsonDataAsync(path);
+
+        return result.StatusCode == HttpStatusCode.OK ?
+          new(true, ApiResultCodes.Success, FromJson<T>(result.Content))
+          : new(false, ToApiRes(result.StatusCode), null);
+      }
+      public async Task<(HttpStatusCode StatusCode, string Content)> GetJsonDataAsync(string path)
+      {
+        RestRequest request = new(path, Method.GET);
+
+        var result = await ExecuteApiAsync(request);
+
+        return (result.StatusCode, result.Content);
+      }
+
+      public async Task<ApiResult<TResult>> ExecuteWithDataAsync<TPost, TResult>(string path, TPost data)
+        where TPost : class
+        where TResult : class
+      {
+        RestRequest request = new(path, Method.POST);
+        var s = ToJson(data);
+        request.AddParameter(application_json, s, ParameterType.RequestBody);
+
+        var result = await ExecuteApiAsync(request);
+
+        return result.StatusCode == HttpStatusCode.OK ?
+          new(true, ApiResultCodes.Success, FromJson<TResult>(result.Content))
+          : new(false, ToApiRes(result.StatusCode), null);
+      }
+      public async Task<ApiResult> ExecuteWithDataAsync<T>(string path, T data) where T : class
+      {
+        RestRequest request = new(path, Method.POST);
+        request.AddJsonBody(data);
+
+        var result = await ExecuteApiAsync(request);
+
+        return new(result.StatusCode == HttpStatusCode.OK, ToApiRes(result.StatusCode));
+      }
+
+      public Task<IRestResponse> ExecuteApiAsync(IRestRequest request)
+      {
+        RestClient client = new(TargetURL);
+        client.Timeout = TimeOut;
+
+        if (!string.IsNullOrWhiteSpace(Token))
+          client.Authenticator = new JwtAuthenticator(Token);
+
+        request.AddHeader("Content-Type", "application/json");
+
+        return client.ExecuteAsync(request);
+      }
+    }
   }
 }
