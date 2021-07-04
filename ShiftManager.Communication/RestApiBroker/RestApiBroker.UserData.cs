a444d86d5a39de5094c4da3a37fb749b04375dc2
ＴＲ <public@technotter.com>
2021-07-04 22:19:58 +0900

    機能を実装

--- ShiftManager.Communication/RestApiBroker/RestApiBroker.UserData.cs
+++ ShiftManager.Communication/RestApiBroker/RestApiBroker.UserData.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 using ShiftManager.DataClasses;
@@ -7,19 +8,24 @@ namespace ShiftManager.Communication
 {
   public partial class RestApiBroker : IInternalApi_UserData
   {
-    public Task<ApiResult<UserSetting>> GetUserSettingAsync() => Task.Run(() =>
-    {
-      return CurrentUserData is null
-      ? new ApiResult<UserSetting>(false, ApiResultCodes.Not_Logged_In, null)
-      : new ApiResult<UserSetting>(true, ApiResultCodes.Success, new(CurrentUserData.UserSetting));
-    });
+    public Task<ApiResult<UserSetting>> GetUserSettingAsync()
+      => throw new NotSupportedException();
 
-    public Task<ApiResult<WorkLog>> GetWorkLogAsync() => Task.Run(() =>
+    public async Task<ApiResult<WorkLog>> GetWorkLogAsync()
     {
-      return CurrentUserData is null
-      ? new ApiResult<WorkLog>(false, ApiResultCodes.Not_Logged_In, null)
-      : new ApiResult<WorkLog>(true, ApiResultCodes.Success, new(CurrentUserData.WorkLog));
-    });
+      if (!IsLoggedIn)
+        return new(false, ApiResultCodes.Not_Logged_In, null);
+
+      var res = await Api.GetDataAsync<RestData.RestWorkLog[]>("/logs");
+
+      if (!res.IsSuccess)
+        return new(false, res.ResultCode, null);
+
+      if (CurrentUserData is null || res.ReturnData is null)
+        return new(false, ApiResultCodes.NewData_Is_NULL, null);
+
+      return new(true, res.ResultCode, new WorkLog(CurrentUserData.UserID, new(res.ReturnData.Select(i => i.ToSingleWorkLog() as ISingleWorkLog).ToDictionary(i => i.AttendanceTime.Date)));
+    }
 
     public Task<ApiResult> UpdatePasswordAsync(IHashedPassword hashedPassword) => Task.Run<ApiResult>(() =>
     {
@@ -29,7 +35,7 @@ namespace ShiftManager.Communication
       return UpdatePasswordAsync(CurrentUserData.UserID, CurrentUserData.FullName, hashedPassword).Result;
     });
 
-    public Task<ApiResult> UpdatePasswordAsync(IUserID userID, INameData nameData, IHashedPassword hashedPassword) => Task.Run<ApiResult>(() =>
+    public Task<ApiResult> UpdatePasswordAsync(IUserID userID, INameData nameData, IHashedPassword hashedPassword) => Task.Run<ApiResult>(async () =>
     {
       if (userID is null || nameData is null || hashedPassword is null)
         return new(false, ApiResultCodes.Invalid_Input); //引数NULLは許容できない
@@ -37,16 +43,12 @@ namespace ShiftManager.Communication
       if (string.IsNullOrWhiteSpace(hashedPassword.Hash) || string.IsNullOrWhiteSpace(hashedPassword.Salt) || hashedPassword.StretchCount <= 0)
         return new(false, ApiResultCodes.Invalid_Input); //ハッシュ情報の不足も許容できない
 
-      if (!TestD.UserDataDictionary.TryGetValue(new(userID), out IUserData? userData) || userData is null)
-        return new(false, ApiResultCodes.UserID_Not_Found);
+      if (userID.Value.Length != 8)
+        return new(false, ApiResultCodes.Invalid_Length_UserID);
 
-      if (new NameData(userData.FullName) != new NameData(nameData))
-        return new(false, ApiResultCodes.FullName_Not_Match);
+      throw new NotSupportedException();
 
-      UserData newUserData = new UserData(userData) with { HashedPassword = new HashedPassword(hashedPassword) };
-      TestD.UserDataDictionary[new(userData.UserID)] = newUserData;
-
-      return SignInAsync(userID, hashedPassword).Result;
+      //return await SignInAsync(userID, hashedPassword);
     });
   }
 }
