d1a1d51a130f2ed5cc140689ba6d06c6b253f905
ＴＲ <public@technotter.com>
2021-07-05 02:07:13 +0900

    Update RestApiBroker.ScheduledShift.cs

--- ShiftManager.Communication/RestApiBroker/RestApiBroker.ScheduledShift.cs
+++ ShiftManager.Communication/RestApiBroker/RestApiBroker.ScheduledShift.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using ShiftManager.Communication.RestData;
 using ShiftManager.DataClasses;
 
 namespace ShiftManager.Communication
@@ -14,25 +15,27 @@ namespace ShiftManager.Communication
     /// <param name="targetDate">取得する予定シフトの対象日</param>
     /// <param name="userID">取得する予定シフトのユーザID</param>
     /// <returns>実行結果</returns>
-    public Task<ApiResult<SingleShiftData>> GetScheduledShiftByIDAsync(DateTime targetDate, IUserID userID) => Task.Run<ApiResult<SingleShiftData>>(() =>
+    public async Task<ApiResult<SingleShiftData>> GetScheduledShiftByIDAsync(DateTime targetDate, IUserID userID)
     {
-      if (!TestD.ScheduledShiftDictionary.TryGetValue(targetDate, out IScheduledShift? scheduledShift) || scheduledShift is null)
-        return new(false, ApiResultCodes.Target_Date_Not_Found, null);
+      if (!IsLoggedIn || CurrentUserData is null)
+        return new(false, ApiResultCodes.Not_Logged_In, null);
 
-      if (!scheduledShift.ShiftDictionary.TryGetValue(new(userID), out ISingleShiftData? singleShiftData) || singleShiftData is null)
-        return new(false, ApiResultCodes.UserID_Not_Found_In_Scheduled_Shift, null);
-      else
-      {
-        ApiResultCodes resultCode = scheduledShift.SchedulingState switch
-        {
-          ShiftSchedulingState.FinalVersion => ApiResultCodes.Success,
-          ShiftSchedulingState.Working => ApiResultCodes.Scheduling_Is_Still_In_Working,
-          ShiftSchedulingState.NotStarted => ApiResultCodes.Scheduling_Is_Not_Started,
-          _ => ApiResultCodes.Unknown_Error,
-        };
-        return new(resultCode != ApiResultCodes.Unknown_Error, resultCode, new(singleShiftData));
+      var res = await Api.GetDataAsync<RestStore>("/stores/0000");
+      if (!res.IsSuccess || res.ReturnData is null)
+        return new(false, ApiResultCodes.Unknown_Error, null);
+
+      if (res.ReturnData.shift_schedules.Length <= 0)
+        return new(false, ApiResultCodes.Data_Not_Found, null);
+
+      var tmp1 = res.ReturnData.shift_schedules.Where(i => i.target_date == targetDate);
+      foreach (var tmp2 in tmp1) {
+        var tmp3 = tmp2.shifts.Where(i => i.user_id == userID.Value).FirstOrDefault();
+        if (tmp3 != default)
+          return new(true, ApiResultCodes.Success, tmp3.ToSingleShiftData());
       }
-    });
+
+      return new(false, ApiResultCodes.Data_Not_Found, null);
+    }
 
     public Task<ApiResult> UpdateRequiredWorkerCountListAsync(DateTime targetDate, IReadOnlyCollection<int> requiredWorkerCountList)
       => Task.Run(() => UpdateScheduledShift(targetDate, (i) => new ScheduledShift(i) with { RequiredWorkerCountList = new(requiredWorkerCountList) }));
@@ -46,6 +49,8 @@ namespace ShiftManager.Communication
 
     private ApiResult UpdateScheduledShift(DateTime targetDate, Func<IScheduledShift, ScheduledShift> DataUpdater)
     {
+      return new(false, ApiResultCodes.Unknown_Error); //実装準備扱い
+      /*
       if (!TestD.ScheduledShiftDictionary.TryGetValue(targetDate, out IScheduledShift? scheduledShift) || scheduledShift is null)
         return new(false, ApiResultCodes.Target_Date_Not_Found);
       var newData = DataUpdater.Invoke(scheduledShift);
@@ -61,7 +66,7 @@ namespace ShiftManager.Communication
       catch (ArgumentNullException)
       {
         return new(false, ApiResultCodes.NewData_Is_NULL);
-      }
+      }*/
     }
   }
 }
