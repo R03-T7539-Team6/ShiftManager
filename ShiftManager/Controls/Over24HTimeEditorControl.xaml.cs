dad3b5516c941f14ea16d781257294c6b231e223
ＴＲ <public@technotter.com>
2021-07-21 07:14:50 +0900

    Add Over24HTimeEditorControl
    CurrentDateTimeへの変更がSource側に反映されないバグあり

---
+++ ShiftManager/Controls/Over24HTimeEditorControl.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ShiftManager.Controls
+{
+  public class Over24HTimeEditorControl : TextBox
+  {
+    public DateTime TargetDate { get => (DateTime)GetValue(TargetDateProperty); set => SetValue(TargetDateProperty, value); }
+    public static readonly DependencyProperty TargetDateProperty = DependencyProperty.Register(nameof(TargetDate), typeof(DateTime), typeof(Over24HTimeEditorControl), new(OnTargetDateChanged));
+
+    public DateTime CurrentDateTime { get => (DateTime)GetValue(CurrentDateTimeProperty); set => SetValue(CurrentDateTimeProperty, value); }
+    public static readonly DependencyProperty CurrentDateTimeProperty = DependencyProperty.Register(nameof(CurrentDateTime), typeof(DateTime), typeof(Over24HTimeEditorControl), new(OnCurrentDateTimeChanged));
+
+    public TimeSpan CurrentTimeSpan { get; private set; }
+
+    public bool IsValidValue { get; private set; }
+
+    static Over24HTimeEditorControl() => DefaultStyleKeyProperty.OverrideMetadata(typeof(Over24HTimeEditorControl), new FrameworkPropertyMetadata(typeof(Over24HTimeEditorControl)));
+
+    static void OnTargetDateChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+      if (d is not Over24HTimeEditorControl c)
+        return;
+
+      DateTime oldDate = (DateTime)e.OldValue;
+      TimeSpan ts = c.CurrentDateTime - oldDate;
+      DateTime newDate = (DateTime)e.NewValue;
+      c.CurrentDateTime = newDate + ts;
+    }
+
+    static void OnCurrentDateTimeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+      if (d is not Over24HTimeEditorControl c)
+        return;
+
+      TimeSpan ts = (DateTime)e.NewValue - c.TargetDate;
+
+      if (ts == c.CurrentTimeSpan)
+        return;
+
+      c.Text = $"{(int)ts.TotalHours:D2}:{(int)ts.TotalMinutes:D2}";
+    }
+
+    protected override void OnTextChanged(TextChangedEventArgs e)
+    {
+      base.OnTextChanged(e);
+      IsValidValue = false;
+
+      if (string.IsNullOrWhiteSpace(Text))
+        return;
+
+      var sarr = Text.Split(':');
+
+      if (sarr.Length != 2 || !int.TryParse(sarr[0], out var hh) || !int.TryParse(sarr[1], out var mm))
+        return;
+
+      TimeSpan ts = new(hh, mm, 0);
+
+      IsValidValue = true;
+
+      if (ts == CurrentTimeSpan)
+        return;
+
+      CurrentTimeSpan = ts;
+      CurrentDateTime = TargetDate + CurrentTimeSpan;
+    }
+  }
+}
