b1297961bcb78cf0b8949f10c835dde85b186837
ＴＲ <public@technotter.com>
2021-08-01 18:12:44 +0900

    実行時引数によるオプション入力に対応 : Resolves #92

--- ShiftManager/App.xaml.cs
+++ ShiftManager/App.xaml.cs
@@ -7,5 +7,15 @@ namespace ShiftManager
   /// </summary>
   public partial class App : Application
   {
+    public static IShiftManagerStartupSettings ShiftManagerStartupSettings { get; private set; }
+    protected override async void OnStartup(StartupEventArgs e)
+    {
+      base.OnStartup(e);
+
+      ShiftManagerStartupSettings settings = new();
+      var ret = await settings.SetFromStringArr(e.Args);
+      ShiftManagerStartupSettings = settings;
+    }
   }
+
 }
